5f2269ee3439a57e2607debe3cc2572cdaee8bde
stropee <stropee@campus-eni.fr>
2023-06-21 11:06:41 +0200

    mod 4 findViewById

---
+++ mod4demofindviewbyid/src/main/java/com/example/mod4demofindviewbyid/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.mod4demofindviewbyid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RatingBar
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+
+private const val TAG = "MainActivity"
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val tv = findViewById<TextView>(R.id.tv_hello)
+        val et = findViewById<EditText>(R.id.et_name)
+        val rating = findViewById<RatingBar>(R.id.ratingBar)
+        val btn = findViewById<Button>(R.id.button)
+
+        tv.text = "Bonjour !"
+        et.setText("Comment ça va ?")
+        rating.rating = 3f
+
+        btn.setBackgroundColor(ContextCompat.getColor(this, R.color.teal_700))
+
+//        btn.setOnClickListener(fun(it){
+//            it.setBackgroundColor(ContextCompat.getColor(this, R.color.black))
+//        })
+
+        btn.setOnClickListener {
+            Log.i(TAG, "onCreate: clic sur le bouton")
+            var txt : String = et.text.toString()
+            Toast.makeText(this, txt, Toast.LENGTH_LONG).show()
+        }
+
+
+    }
+}
