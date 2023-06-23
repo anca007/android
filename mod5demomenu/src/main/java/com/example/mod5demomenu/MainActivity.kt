619735008f1868f8174bda3ef1f25dfc390eeecd
stropee <stropee@campus-eni.fr>
2023-06-23 16:34:27 +0200

    mod 5 ajout de menu

---
+++ mod5demomenu/src/main/java/com/example/mod5demomenu/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.mod5demomenu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    //permet affichage du menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.itemAdd -> println("Add")
+            R.id.itemEdit -> println("Edit")
+            R.id.itemDelete -> println("Delete")
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
